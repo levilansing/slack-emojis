265173d36670016733c66ce513e60a3ebcefd739
Levi Lansing <levi.lansing@compass.com>
2022-03-22 13:37:15 -0400

    make markdown more readable on github

--- createMarkdown.js
+++ createMarkdown.js
@@ -2,21 +2,44 @@ const fs = require("fs");
 const path = require("path");
 const emojiData = require("./emojis.json");
 
-const fileName = (emoji) =>
-  "emojis/" + emoji.name + (emoji.url.match(/\.[^.]+$/) || "");
+const imageTag = (emoji) => {
+  const path = "emojis/" + emoji.name + (emoji.url.match(/\.[^.]+$/) || "");
+  return `<img src="${path}" max-width="100px">`;
+};
+
+const cleanName = (emoji) => {
+  const name = emoji.name.replace(/[_\-]/g, "-");
+  return name.split("").reduce(
+    ([out, n], char, i) => {
+      if (!char.match(/[\s\-]/)) {
+        if (n >= 16 && i !== name.length - 1) {
+          out += "-";
+          n = 0;
+        } else {
+          n++;
+        }
+      } else {
+        n = 0;
+      }
+      out += char;
+      return [out, n];
+    },
+    ["", 0]
+  )[0];
+};
 
 // generate markdown table of all emojis
+const tableWidth = 4;
 const table = [
-  "| Emoji | Name | Emoji | Name | Emoji | Name | Emoji | Name ",
-  "| :---: | :--- | :---: | :--- | :---: | :--- | :---: | :--- ",
+  "| Name | Emoji ".repeat(tableWidth),
+  "| ---: | :---: ".repeat(tableWidth),
 ];
 const emojis = emojiData.emoji;
-const tableWidth = 4;
 for (let i = 0; i < emojis.length; i += tableWidth) {
   table.push(
     emojis
       .slice(i, i + tableWidth)
-      .map((emoji) => `| ![${emoji.name}](${fileName(emoji)}) | ${emoji.name} `)
+      .map((emoji) => `| ${cleanName(emoji)} | ${imageTag(emoji)} `)
       .join("")
   );
 }
